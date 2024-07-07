bb39b34510395ec2d4ed91bd59c193f667ddc534
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-06 21:28:21 -0600

    Vídeo 228 - Creando nuestro primer store

---
+++ src/stores/bebidas.js
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+export const useBebidasStore = defineStore('bebidas', (() => {
+    const categorias = ref([])
+    return {
+        categorias
+    }
+}))
