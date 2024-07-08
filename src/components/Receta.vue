4e83b7ffde4438519c93ee457179dcc7bef5b581
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-07 18:54:12 -0600

    Vídeo 236 - Finalizando el componente que muestra la bebida.

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -9,7 +9,13 @@ defineProps(
 </script>
 
 <template>
-    <div>
-        <h2>{{ receta.strDrink }}</h2>
+    <div class="border shadow-lg">
+        <div class="overflow-hidden">
+            <img class="hover:scale-125 transition-transform hover:rotate-2" :alt="'Imagen de ' + receta.strDrink" :src="receta.strDrinkThumb" />
+        </div>
+        <div class="p-5">
+            <h2 class="text-2xl truncate font-extrabold">{{ receta.strDrink }}</h2>
+            <button type="button" class="bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg hover:bg-orange-300">>Ver Receta</button>
+        </div>
     </div>
 </template>
