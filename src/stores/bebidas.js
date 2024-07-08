32954771801695fa46d6fb6a5e9239c42c03d5fa
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-07 18:44:56 -0600

    Vídeo 235 - Primeros pasos mostrando los resultados de la consulta.

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -24,6 +24,7 @@ export const useBebidasStore = defineStore('bebidas', (() => {
     return {
         categorias,
         busqueda,
+        recetas,
         obtenerRecetas
     }
 }))
--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { useBebidasStore } from '../stores/bebidas';
+import Receta from '../components/Receta.vue'
+const bebidas = useBebidasStore()
 </script>
 
 <template>
-    <div>
-        <h1 class="text-6xl font-extrabold">Recetas</h1>
+
+    <h1 class="text-6xl font-extrabold">Recetas</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+        <Receta v-for="receta in bebidas.recetas" :key="receta.idDrink" :receta="receta" />
     </div>
 </template>
