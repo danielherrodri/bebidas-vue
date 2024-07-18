de198c0bf90afa7e43c8d54cebae6065de98e524
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-17 22:31:47 -0600

    Video 253 - Notificaciones en Vue.js

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from 'vue-router';
 import Header from './components/Header.vue';
 import Modal from './components/Modal.vue';
+import Notificacion from './components/Notificacion.vue'
 </script>
 
 <template>
@@ -11,4 +12,5 @@ import Modal from './components/Modal.vue';
   </main>
 
   <Modal />
+  <Notificacion />
 </template>
