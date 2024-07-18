c86d83ddef51eb57c0c14338b061f5c02a216ca9
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-17 21:33:33 -0600

    Video 244 - Agregando recetas a favoritos

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,9 +1,14 @@
 import { defineStore } from "pinia";
+import { useBebidasStore } from "./bebidas";
+import { ref } from "vue";
 export const useFavoritosStore = defineStore('favoritos', () => {
+    const bebidas = useBebidasStore()
+    const favoritos = ref([])
     const handleClickFavorito = () => {
-        console.log('Agregando')
+        favoritos.value.push(bebidas.receta)
     }
     return {
+        favoritos,
         handleClickFavorito
     }
 })
