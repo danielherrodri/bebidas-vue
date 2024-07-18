8e664fc556a69da7683a1a133f3fb3f33059bad7
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-17 21:40:35 -0600

    Video 245 - Almacenando favoritos en localStorage

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,9 +1,20 @@
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
 export const useFavoritosStore = defineStore('favoritos', () => {
     const bebidas = useBebidasStore()
     const favoritos = ref([])
+
+    watch(favoritos, () => {
+        sincronizarLocalStorage()
+    }, {
+        deep: true
+    })
+
+    const sincronizarLocalStorage = () => {
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+    }
+
     const handleClickFavorito = () => {
         favoritos.value.push(bebidas.receta)
     }
